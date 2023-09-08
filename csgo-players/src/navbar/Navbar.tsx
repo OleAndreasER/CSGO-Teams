b183381d3b6fa58147e6883166cef790931c2dbd
Andreas Li <andrea10@stud.ntnu.no>
2023-09-08 11:13:37 +0200

    add navbar

---
+++ csgo-players/src/navbar/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Navbar.css"
+
+function Navbar() {
+  return (
+    <header>
+      <nav className="nav">
+        <ul className="nav-items">
+          <li> Favorites </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
