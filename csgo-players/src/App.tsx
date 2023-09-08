c56545814b960d0e2208c0cce3d9eee09115c72b
oarorvik <oarorvik@stud.ntnu.no>
2023-09-08 12:50:41 +0200

    add useTeams hook

--- csgo-players/src/App.tsx
+++ csgo-players/src/App.tsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useTeams } from "./teams/use-teams";
 
-function App() {
-  const [count, setCount] = useState(0)
+export function App() {
+  const teams = useTeams();
 
+  if (teams === undefined) return <></>;
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      {teams[0].name}
+      <img src={teams[0].logo} />
+    </div>
+  );
 }
-
-export default App
