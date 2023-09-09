a1bf75e8580c098a9aaa9e3bec7e6fed503dd61c
Andreas Lilleby Hjulstad <andrelhj@stud.ntnu.no>
2023-09-09 15:36:49 +0200

    update imports in app

--- csgo-players/src/app/app.tsx
+++ csgo-players/src/app/app.tsx
@@ -1,8 +1,8 @@
 import './app.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Navbar } from '../navbar/navbar';
-import { Mainpage } from '../mainpage/mainpage';
-import { Favorites } from '../favorites/favorites';
+import Navbar from '../navbar/navbar';
+import Mainpage from '../mainpage/mainpage';
+import Favorites from '../favorites/favorites';
 
 export function App() {
 
