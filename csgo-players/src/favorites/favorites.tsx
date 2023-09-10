c7136ec472a1f3eb55f873f6bbdee5f82e691be6
oarorvik <oarorvik@stud.ntnu.no>
2023-09-10 16:50:59 +0200

    add favorites provider
    Provides isFavorite(team) and toggleFavorite(team).

--- csgo-players/src/favorites/favorites.tsx
+++ csgo-players/src/favorites/favorites.tsx
@@ -1,9 +1,24 @@
-import "./favorites.css"
+import { useContext } from "react";
+import "./favorites.css";
+import { FavoritesContext } from "./favorites-provider";
+import { useTeams } from "../teams/use-teams";
 
 export default function Favorites() {
+  const { isFavorite, toggleFavorite } = useContext(FavoritesContext);
+  const teams = useTeams();
+
+  if (teams === undefined) return <div></div>;
   return (
     <div>
-      Favorite
+      <div>
+        {isFavorite(teams[4])
+          ? "Team[4] is a favorite"
+          : "Team[4] is not a favorite"}
+      </div>
+
+      <button onClick={() => toggleFavorite(teams[4])}>
+        Toggle team[4] favorite
+      </button>
     </div>
   );
 }
--- csgo-players/src/main.tsx
+++ csgo-players/src/main.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { App } from "./app/app";
+import { FavoritesProvider } from "./favorites/favorites-provider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={new QueryClient()}>
-      <App />
+      <FavoritesProvider>
+        <App />
+      </FavoritesProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
